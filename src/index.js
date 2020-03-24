58dbb68c70e8dfa74ece0f5acee34d45c375daa6
zeke <emostov@middlebury.edu>
2020-03-24 16:54:35 -0700

    add getblock

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
+import { ApiPromise, WsProvider } from '@polkadot/api';
+
+import { subscribeToBlockHeaders, headers } from '../src/polkadot_api_util';
+
+const wsProvider = new WsProvider('wss://kusama-rpc.polkadot.io/');
+
+
 window.addEventListener('DOMContentLoaded', (event) => {
   console.log('hiii');
+
+  ApiPromise
+    .create({ provider: wsProvider })
+    .then((api) => {
+      subscribeToBlockHeaders(api);
+      // console.log(headers);
+    });
 });
 
--- src/polkadot_api_util.js
+++ src/polkadot_api_util.js
@@ -1,4 +1,25 @@
-import { ApiPromise, WsProvider } from '@polkadot/api';
+// import { ApiPromise, WsProvider } from '@polkadot/api';
 
-const wsProvider = new WsProvider('wss://kusama-rpc.polkadot.io/');
-const api = await ApiPromise.create({ provider: wsProvider });
+export const headers = {};
+
+export function subscribeToBlockHeaders(api) {
+  // api.rpc.chain.subscribeNewHeads((lastHeader) => {
+  //   headers[lastHeader.number] = lastHeader;
+  //   // console.log('subscribe to head', `${lastHeader.number}`);
+  //   console.log(`#${lastHeader.number} has hash ${lastHeader.hash}`);
+  //   console.log(lastHeader.digest.logs);
+  // });
+
+  api.rpc.chain.getBlock((block) => {
+    // headers[lastHeader.number] = lastHeader;
+    console.log(block);
+    console.log(block.toString());
+    // console.log('subscribe to head', `${lastHeader.number}`);
+    // console.log(`#${lastHeader.number} has hash ${lastHeader.hash}`);
+    // console.log(lastHeader.digest.logs);
+  });
+}
+
+export async function getAddressInfor() {
+  return -1;
+}
