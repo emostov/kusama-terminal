ece15bba210ef9eb46f53aa80e0f5a46472dd88a
zeke <emostov@middlebury.edu>
2020-03-24 14:24:51 -0700

    set up content loaded

--- dist/main.js
+++ dist/main.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("window.addEventListener('DOMContentLoaded', (event) => {\n  console.log('hiii');\n})\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
