0b9e6ea8cca9f1299c1b2b1668255c256db0e78d
zeke <emostov@middlebury.edu>
2020-03-24 19:01:20 -0700

    figure out how to subscribe to blocks

--- src/polkadot_api_util.js
+++ src/polkadot_api_util.js
@@ -1,23 +1,21 @@
 // import { ApiPromise, WsProvider } from '@polkadot/api';
 
 export const headers = {};
+export const blocks = {}
 
 export function subscribeToBlockHeaders(api) {
-  // api.rpc.chain.subscribeNewHeads((lastHeader) => {
-  //   headers[lastHeader.number] = lastHeader;
-  //   // console.log('subscribe to head', `${lastHeader.number}`);
-  //   console.log(`#${lastHeader.number} has hash ${lastHeader.hash}`);
-  //   console.log(lastHeader.digest.logs);
-  // });
+  api.rpc.chain.subscribeNewHeads((lastHeader) => {
+    headers[lastHeader.number] = lastHeader;
 
-  api.rpc.chain.getBlock((block) => {
-    // headers[lastHeader.number] = lastHeader;
-    console.log(block);
-    console.log(block.toString());
-    // console.log('subscribe to head', `${lastHeader.number}`);
-    // console.log(`#${lastHeader.number} has hash ${lastHeader.hash}`);
-    // console.log(lastHeader.digest.logs);
+    api.rpc.chain.getBlock(lastHeader.hash, (block) => {
+
+      console.log(block.toString());
+
+    });
+
   });
+
+
 }
 
 export async function getAddressInfor() {
