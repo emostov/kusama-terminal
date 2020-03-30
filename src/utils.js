4c7db926e401a09f32585c04ce43aa4d98a33170
zeke <emostov@middlebury.edu>
2020-03-30 10:12:25 -0700

    add scroll to

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,5 @@
 
 // https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro
-// eslint-disable-next-line import/prefer-default-export
 export function stringToNode(inner) {
   const template = document.createElement('template');
   const html = inner.trim();
